1dfa9d41ef638fb0d0aa27012a179870bd59cc38
Steven Shih <stevenshih1997@gmail.com>
2019-01-26 21:18:50 -0800

    search function

--- client/src/components/search/Search.jsx
+++ client/src/components/search/Search.jsx
@@ -2,71 +2,60 @@ import React, { Component } from 'react';
 import SearchSong from './SearchSong.jsx';
 import { MDBCol } from "mdbreact";
 import './Search.scss';
-import { Page, List, ListHeader, ListItem } from 'react-onsenui';
+import { SearchInput, Page, List, ListHeader, ListItem } from 'react-onsenui';
 import 'onsenui/css/onsenui.css';
 import 'onsenui/css/onsen-css-components.css';
 
 class Search extends Component {
-    state = { 
-        isSearch: true,
-        songs: [
-            {
-                songTitle: 'This is a Test',
-                songArtist: 'Test Artist',
-                songImage: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/dd/Dark_Horse_%28Nickelback_album_cover%29.jpg/220px-Dark_Horse_%28Nickelback_album_cover%29.jpg'
-            },
-            {
-                songTitle: 'This is a Test',
-                songArtist: 'Test Artist',
-                songImage: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/dd/Dark_Horse_%28Nickelback_album_cover%29.jpg/220px-Dark_Horse_%28Nickelback_album_cover%29.jpg'
-            },
-            {
-                songTitle: 'This is a Test',
-                songArtist: 'Test Artist',
-                songImage: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/dd/Dark_Horse_%28Nickelback_album_cover%29.jpg/220px-Dark_Horse_%28Nickelback_album_cover%29.jpg'
-            },
-            {
-                songTitle: 'This is a Test',
-                songArtist: 'Test Artist',
-                songImage: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/dd/Dark_Horse_%28Nickelback_album_cover%29.jpg/220px-Dark_Horse_%28Nickelback_album_cover%29.jpg'
-            },
-            {
-                songTitle: 'This is a Test',
-                songArtist: 'Test Artist',
-                songImage: 'https://i.scdn.co/image/366f54da73f053d324aad6ca819a103ddb0e64bc'
-            }
-        ]
+    constructor() {
+        super();
+        this.state = { 
+            isSearch: true,
+            songs: [
+                {
+                    songTitle: 'This is a Test',
+                    songArtist: 'Test Artist',
+                    songImage: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/dd/Dark_Horse_%28Nickelback_album_cover%29.jpg/220px-Dark_Horse_%28Nickelback_album_cover%29.jpg'
+                },
+                {
+                    songTitle: 'This is a Test',
+                    songArtist: 'Test Artist',
+                    songImage: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/dd/Dark_Horse_%28Nickelback_album_cover%29.jpg/220px-Dark_Horse_%28Nickelback_album_cover%29.jpg'
+                },
+                {
+                    songTitle: 'This is a Test',
+                    songArtist: 'Test Artist',
+                    songImage: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/dd/Dark_Horse_%28Nickelback_album_cover%29.jpg/220px-Dark_Horse_%28Nickelback_album_cover%29.jpg'
+                },
+                {
+                    songTitle: 'This is a Test',
+                    songArtist: 'Test Artist',
+                    songImage: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/dd/Dark_Horse_%28Nickelback_album_cover%29.jpg/220px-Dark_Horse_%28Nickelback_album_cover%29.jpg'
+                },
+                {
+                    songTitle: 'This is a Test',
+                    songArtist: 'Test Artist',
+                    songImage: 'https://i.scdn.co/image/366f54da73f053d324aad6ca819a103ddb0e64bc'
+                }
+            ]
+        }
     }
     render() { 
-        return ( 
-            <React.Fragment>
-                <div id="spartyfy-search-header" className="container">
-                    <div className="row d-flex justify-content-center">
-                        <h3>{this.formatTitle()}</h3>
-                    </div>
-                    <div className="row d-flex justify-content-center">
-                        <MDBCol md="6">
-                            <input className="form-control" type="text" placeholder="Search" aria-label="Search" />
-                        </MDBCol>
-                    </div>
-                    {this.renderSongs()}
-                </div>
-            </React.Fragment>
-        );
-    }
-
-    formatTitle() {
-        return (this.state.isSearch) ? 'Search' : 'Top Songs';
-    }
-    
-    renderSongs() {
         return (
-            <React.Fragment>
-                {this.state.songs.map(song => <div className="row"><SearchSong song={song} /></div>)}
-            </React.Fragment>    
+            <div>
+                <SearchInput
+                style={{width: "100%"}}
+                value={this.state.text}
+                onChange={(event) => { this.setState({text: event.target.value, isSearch: false})} }
+                modifier='material'
+                placeholder='Search' />
+                <List
+                    dataSource={this.state.songs}
+                    renderRow={(row, idx) => <SearchSong id={idx} songTitle={row.songTitle} songArtist={row.songArtist} songImage={row.songImage}/>}
+                />
+            </div>
         );
     }
-
     /* TODO: add an on-init fetch for Spotify top songs */
 }
  
