079bd3bf55d3f234f825142a2145b5a03d4ec6c7
Cowan Cumming <ctcumming@gmail.com>
2019-01-26 23:01:17 -0800

    Removed sample data.

--- client/src/components/search/Search.jsx
+++ client/src/components/search/Search.jsx
@@ -14,33 +14,7 @@ class Search extends Component {
         super();
         this.state = { 
             isSearch: true,
-            songs: [
-                {
-                    songTitle: 'This is a Test',
-                    songArtist: 'Test Artist',
-                    songImage: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/dd/Dark_Horse_%28Nickelback_album_cover%29.jpg/220px-Dark_Horse_%28Nickelback_album_cover%29.jpg'
-                },
-                {
-                    songTitle: 'This is a Test',
-                    songArtist: 'Test Artist',
-                    songImage: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/dd/Dark_Horse_%28Nickelback_album_cover%29.jpg/220px-Dark_Horse_%28Nickelback_album_cover%29.jpg'
-                },
-                {
-                    songTitle: 'This is a Test',
-                    songArtist: 'Test Artist',
-                    songImage: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/dd/Dark_Horse_%28Nickelback_album_cover%29.jpg/220px-Dark_Horse_%28Nickelback_album_cover%29.jpg'
-                },
-                {
-                    songTitle: 'This is a Test',
-                    songArtist: 'Test Artist',
-                    songImage: 'https://upload.wikimedia.org/wikipedia/en/thumb/d/dd/Dark_Horse_%28Nickelback_album_cover%29.jpg/220px-Dark_Horse_%28Nickelback_album_cover%29.jpg'
-                },
-                {
-                    songTitle: 'This is a Test',
-                    songArtist: 'Test Artist',
-                    songImage: 'https://i.scdn.co/image/366f54da73f053d324aad6ca819a103ddb0e64bc'
-                }
-            ]
+            songs: []
         };
 
         this.searchSong = this.searchSong.bind(this);
