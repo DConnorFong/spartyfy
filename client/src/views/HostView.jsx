9a2c0868771838b3467c24d0788e8c76f9f3d01a
Connor Fong <connor_fong@hotmail.com>
2019-01-26 17:15:48 -0800

    Added routing and views directory

---
+++ client/src/views/HostView.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class HostView extends Component {
+    state = {  }
+    render() { 
+        return ( <h1>HostView</h1> );
+    }
+}
+ 
+export default HostView;
