05ac0dfe3a6ebdddd9414cf06ec72609063c0276
Cowan Cumming <ctcumming@gmail.com>
2019-01-27 00:19:50 -0800

    Client can now add songs to server playlist.

--- client/src/components/search/SearchSong.jsx
+++ client/src/components/search/SearchSong.jsx
@@ -9,12 +9,13 @@ class SearchSong extends Component {
         songTitle: this.props.songTitle,
         songArtist: this.props.songArtist,
         songImage: this.props.songImage,
-        id: this.props.id
-    }
+        id: this.props.id,
+        onClick: this.props.onClick
+    };
 
     render() { 
         return (
-            <ListItem key={this.props.id} tappable>
+            <ListItem key={this.props.id} tappable onClick={this.props.onClick}>
                 <div className="center">
                     {this.state.songTitle}<br/>
                     {this.state.songArtist}
